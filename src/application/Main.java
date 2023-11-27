58aa32860e5aaea10519cd5ba0c08a8bfb58f16a
Gabriel Santos <gabrielcarneirocsantos@gmail.com>
2023-11-27 13:12:16 -0300

    Negando ao criar arquivo

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,9 +2,22 @@ package application;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        String path = "c:\\temp\\";
 
+        String[] list = new String[] {"Oi", "Tudo bem?", "Tchau!"};
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+            for(String str : list){
+                bw.write(str);
+                bw.newLine();
+            }
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
